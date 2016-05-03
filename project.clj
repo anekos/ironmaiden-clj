79c44462b8a86c4bbdefdfe6fbcd464ea583a1d5
anekos <anekos@snca.net>
2016-05-04 01:24:37 +0900

    Add input-event

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "The Input Manager"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [bytebuffer "0.2.0"]]
   :main ironmaiden.core
   :aot [ironmaiden.core]
   :jar-name "ironmaiden.jar"
---
+++ src/clojure/ironmaiden/device/input_event.clj
@@ -0,0 +1,43 @@
+(ns ironmaiden.device.input-type
+  (:require [clojure.string :as string]
+            [bytebuffer.buff :refer :all]))
+
+
+(comment
+ (define-alien-type
+   nil
+   (struct timeval
+           (sec (unsigned 64))
+           (usec (unsigned 64))))
+
+ (define-alien-type
+   nil
+   (struct input-event-struct
+           (time (struct timeval))
+           (type (unsigned 16))
+           (code (unsigned 16))
+           (value (integer 32)))))
+
+
+(defrecord InputEvent [sec usec type code value])
+
+
+(defn read-input-event
+  ([fin bb]
+    (.read fin (.array bb))
+    (let [result (read-input-event bb)]
+      (.rewind bb)
+      result))
+  ([bb]
+    (let [[sec usec type code value] (unpack bb "LLSSi")]
+      (InputEvent. sec usec type code value))))
+
+
+(defn input-event-seq [filepath]
+  (let [fin (java.io.FileInputStream. filepath)
+        bb (byte-buffer 24)]
+    (letfn [(read1 []
+              (cons
+                (read-input-event fin bb)
+                (lazy-seq (read1))))]
+      read1)))
